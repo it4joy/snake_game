644ed9c11f1be3b93051b0f373eecfd78a7963f1
it4joy <drkierkegor@gmail.com>
2015-12-06 20:21:06 +0300

    Educational project on C#

--- Program.cs
+++ Program.cs
@@ -10,7 +10,26 @@ namespace snake_game
     {
         static void Main(string[] args)
         {
-            
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.symb = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.symb = '#';
+            p2.Draw();
+
+            Point p3 = new Point();
+            p3.x = 10;
+            p3.y = 20;
+            p3.symb = 'C';
+            p3.Draw();
+
+            // test for GitHub Desktop;
+
             Console.ReadLine();
         }
     }
