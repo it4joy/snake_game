e1911a2e8dfd930c35e535f7aa18da49f8cbfa94
it4joy <drkierkegor@gmail.com>
2015-12-08 13:15:51 +0300

    Educational project on C#
    Work with class 'List'.

--- Program.cs
+++ Program.cs
@@ -19,6 +19,26 @@ namespace snake_game
             Point p3 = new Point(10, 20, 'C');
             p3.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add( 0 );
+            numList.Add( 1 );
+            numList.Add( 2 );
+
+            int x = numList[ 0 ];
+            int y = numList[ 1 ];
+            int z = numList[ 2 ];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine( i );
+            }
+
+            numList.RemoveAt( 0 );
+
+            List<Point> pList = new List<Point>();
+            pList.Add( p1 );
+            pList.Add( p2 );
+
             Console.ReadLine();
         }
     }
